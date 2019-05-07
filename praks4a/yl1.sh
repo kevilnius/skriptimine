452ded92ccf7b4955bbbb0689a40500821446e19
Kevin Sakeus <kevin.sakeus@khk.ee>
2019-05-07 02:26:15 -0400

    Tuletab meelde vanus

--- praks4a/yl1.sh
+++ praks4a/yl1.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #randmel 4 limpa pudelit 
 echo -n "Sisestage siia oma nimi: "
+# sisestades nimie jäetakse see meelde
 read nimi
 echo "Tere,tulemast $nimi"
+# kuvatakse teie nimi
 echo -n "Sisestage siia oma sünniaasta:"
+#sisestades sünniaasta saate teada kui vana olete
 read vanus
 h=`date +"%Y"`
 y=`expr $h - $vanus`
 echo "$nimi näib te olete $y aastat vana"
-#
+# kuvatakse kui vana olete
 #
 #skripti lõpp
