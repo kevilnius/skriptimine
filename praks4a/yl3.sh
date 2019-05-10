143cab0e076a8cacb427f370f526f2c394c08e3c
Kevin Sakeus <kevin.sakeus@khk.ee>
2019-05-10 02:03:16 -0400

    Kokkupakkimine

--- praks4a/yl3.sh
+++ praks4a/yl3.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
-# Mida tahate backupida
-echo -n " Kirjutake  directory nimi  mida on vaja  backupida
-read joink
-#määrake kuhu kataloog pannakse
-echo -n "kuhu soovite kataloogi paigutada: "
-read dest
-#vaja on teada arhiivi nimi
-failinimi="backup.tar.gz"
-#tar failid mida vajame
-tar -czf $failinimi $backup
-cp $failinimi $dest
-#kuvab vajatud info
-echo "Sinu backup fail on kokku surutud"
+#
+#
+tar -czf /home/user/skriptimine/praks4a/./backup/test.tar.gz
+echo  "kataloogi /home/user/skriptimine/praks4a/test backupi nimi on test.tar.gz ning asub /home/user/skriptimine/praks4a/backup kaustas "
+#kuvatakse  kokkupakitud tulemus
 #
 #skripti lõpp
  
